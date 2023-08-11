8da3ed18ab2b21857d7f467d58a316eec16b4846
Samuel Kinyanjui <sax80kinya@gmail.com>
2023-08-11 14:54:12 +0300

    Updated the getdata
    Fixed the linter errors in the get method

--- src/module/getdata.js
+++ src/module/getdata.js
@@ -10,7 +10,7 @@ const getData = () => {
       document.querySelector(
         'ul',
       ).innerHTML += ` <li><span>${user.user} </span>: <span>${user.score}</span></li> `;
-    })
+    });
   });
 };
 
