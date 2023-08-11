93c2da62957e47a3accf448d37f1643d708c62eb
Samuel Kinyanjui <sax80kinya@gmail.com>
2023-08-11 14:20:18 +0300

    updated
    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import './style.css';
 import {  submitting } from './module/Add.js';
 import getData from './module/getdata.js';
-
-//const submit = document.querySelector('.submit');
-console.log('test');
 getData();
 submitting();
